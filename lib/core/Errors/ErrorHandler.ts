27d0476f77cc54fedc5469827061ce1beab6b1f9
Sami EL KATEB <sami.elkateb@gmail.com>
2022-08-23 18:45:07 +0200

    fix: linting Git and errors

--- lib/core/Errors/ErrorHandler.ts
+++ lib/core/Errors/ErrorHandler.ts
@@ -2,25 +2,23 @@ import { ZodError } from 'zod';
 import { checkIsObject, checkIsString, hasOwnProperty } from '../../utils/validations/basicTypeValidations';
 import Log from '../Logger';
 import GitlabApiError from './GitlabApiError';
+
 const logger = new Log();
 class ErrorHandler {
-	constructor() {}
-	static try = async (callback: Function) => {
-		try {
-			await callback();
-		} catch (error: unknown) {
-			if (error instanceof ZodError) return error.errors.forEach(e => logger.error(e.message));
-			if (typeof error === 'string') logger.error(error)
-			if (error instanceof Error) logger.error(error.message);
-			if (error instanceof TypeError)
-				logger.error(error.message, 'Type Error');
-			if (error instanceof GitlabApiError)
-				logger.error(error.message, 'Gitlab Api Error');
+  static try = async (callback: Function) => {
+    try {
+      await callback();
+    } catch (error: unknown) {
+      if (error instanceof ZodError) error.errors.forEach((e) => logger.error(e.message));
+      if (typeof error === 'string') logger.error(error);
+      if (error instanceof Error) logger.error(error.message);
+      if (error instanceof TypeError) { logger.error(error.message, 'Type Error'); }
+      if (error instanceof GitlabApiError) { logger.error(error.message, 'Gitlab Api Error'); }
 
-			if(checkIsObject(error) && hasOwnProperty(error, 'stack') && checkIsString(error.stack)) {
-				logger.stack(error.stack)
-			}
-		}
-	};
+      if (checkIsObject(error) && hasOwnProperty(error, 'stack') && checkIsString(error.stack)) {
+        logger.stack(error.stack);
+      }
+    }
+  };
 }
 export default ErrorHandler;
--- lib/core/Errors/GitlabApiError.ts
+++ lib/core/Errors/GitlabApiError.ts
@@ -1,55 +1,58 @@
 import { z } from 'zod';
 import ERROR_MESSAGES from '../../constants/ErrorMessages';
 import {
-    assertArray,
-    assertObject,
-    assertProperty,
-    assertString,
+  assertArray,
+  assertObject,
+  assertProperty,
+  assertString,
 } from '../../utils/assertions/baseTypeAssertions';
 import {
-    checkIsObject,
-    checkIsString,
-    hasOwnProperty,
+  checkIsObject,
+  checkIsString,
+  hasOwnProperty,
 } from '../../utils/validations/basicTypeValidations';
 
 class GitlabApiError {
-    readonly message: string;
-    constructor(error: unknown) {
-        try {
-            const errorSchema = z.object({
-                response: z.object({ data: z.string() }),
-            });
-            const parsedError = errorSchema.parse(error);
-            this.message = GitlabApiError.getMessageFromData(
-                parsedError.response.data
-            );
-        } catch (error) {
-            this.message = ERROR_MESSAGES.unknownGitlabApiError;
-        }
+  readonly message: string;
+
+  constructor(gitlabError: unknown) {
+    try {
+      const errorSchema = z.object({
+        response: z.object({ data: z.string() }),
+      });
+      const parsedError = errorSchema.parse(gitlabError);
+      this.message = GitlabApiError.getMessageFromData(
+        parsedError.response.data,
+      );
+    } catch (error) {
+      this.message = ERROR_MESSAGES.unknownGitlabApiError;
     }
-    static getMessageFromData = (data: unknown) => {
-        assertObject(data);
-        if (hasOwnProperty(data, 'error')) {
-            assertString(data.error);
-            return data.error;
-        }
-        assertProperty(data, 'message');
-        if (checkIsString(data.message)) {
-            return data.message;
-        }
-        if (
-            checkIsObject(data.message) &&
-            hasOwnProperty(data.message, 'base')
-        ) {
-            return GitlabApiError.getMessageFromArray(data.message.base);
-        }
-        return GitlabApiError.getMessageFromArray(data.message);
-    };
-    static getMessageFromArray = (messages: unknown) => {
-        assertArray(messages);
-        const message = messages[0];
-        assertString(message);
-        return message;
-    };
+  }
+
+  static getMessageFromData = (data: unknown) => {
+    assertObject(data);
+    if (hasOwnProperty(data, 'error')) {
+      assertString(data.error);
+      return data.error;
+    }
+    assertProperty(data, 'message');
+    if (checkIsString(data.message)) {
+      return data.message;
+    }
+    if (
+      checkIsObject(data.message)
+            && hasOwnProperty(data.message, 'base')
+    ) {
+      return GitlabApiError.getMessageFromArray(data.message.base);
+    }
+    return GitlabApiError.getMessageFromArray(data.message);
+  };
+
+  static getMessageFromArray = (messages: unknown) => {
+    assertArray(messages);
+    const message = messages[0];
+    assertString(message);
+    return message;
+  };
 }
 export default GitlabApiError;
